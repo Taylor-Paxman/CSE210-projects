682c782f7c56662725923541a149ae98df5828e3
Your Name <your.email@example.com>
2026-09-18 18:24:31 -0600

    Created the Grade Test

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,79 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Ask the User their grade Percentage
+
+        Console.Write("What is your grade percentage? ");
+        string valuefromuser = Console.ReadLine();
+        int percent = int.Parse(valuefromuser);
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+            // Console.WriteLine($"Your Grade is: {letter}, You passed ");
+            }
+        
+        else if (percent >= 80)
+        {
+            letter = "B";
+            // Console.WriteLine($"Your Grade is: {letter}, You passed ");
+            }
+
+        else if (percent >= 70)
+        {
+            letter = "C";
+            // Console.WriteLine($"Your Grade is: {letter}, You passed ");
+            }
+
+        else if (percent >= 60)
+        {
+            letter = "D";
+            // Console.WriteLine($"Your Grade is: {letter}, Sorry Better Try again ");
+        
+            }
+
+        else 
+        {
+            letter = "F";
+            
+            // Console.WriteLine($"Your Grade is: {letter}, You FAILED ");
+            }
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine($"Your Grade is: {letter}, You passed ");
+        }
+        else
+
+        {
+          Console.WriteLine($"Your Grade is: {letter}, You FAILD, Better try again ");
+          
+        }
+
+
+        
+        
+            
+            
+        
+
+    
+
+        
+            
+       
+        
+
+    
+
+
+
+
+
     }
 }
