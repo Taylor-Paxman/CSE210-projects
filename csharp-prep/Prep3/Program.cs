16822634c6556e91894e88cc210ad3fd962d30b6
Your Name <your.email@example.com>
2026-09-19 15:03:12 -0600

    Created game and tested

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        
+        Random r = new Random();
+        int winnum = r.Next(0,100);
+        bool win = false;
+        
+        do
+        {
+            Console.WriteLine("Guess a number between 0 and 100: ");
+            string s = Console.ReadLine();
+            int i = int.Parse(s);
+
+            if ( i > winnum)
+            {
+                Console.WriteLine("to high! guess lower");
+            }
+            else if ( i < winnum)
+            {
+                Console.WriteLine("to low! guess higher");
+            }
+            else if ( i == winnum)
+            {
+                Console.WriteLine("You Win!!!");
+            }
+
+            Console.WriteLine();
+        } while(win == false);
+    }   
 }
+
+
