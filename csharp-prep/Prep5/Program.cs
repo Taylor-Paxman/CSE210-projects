709449d567d928ec6a23f3f1b3afaef3f7678a88
Your Name <your.email@example.com>
2026-09-20 12:23:24 -0600

    Minor Adjustments

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        PromptUserName();
+        PromptUserNumber();
+        PromtUserBirthYear();
+        
+        
+        
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to my program");
+    }
+    static void PromptUserName()
+    {
+        Console.Write("What is your full name?: ");
+        string name = Console.ReadLine();
+        
+    }
+    static void PromptUserNumber()
+    {
+        Console.Write("What is your Favorite Number?: ");
+        int number = int.Parse(Console.ReadLine());
+    }
+    static void PromtUserBirthYear()
+    {
+        Console.Write("What Year were you born?: ");
+        int birthyear = int.Parse(Console.ReadLine());
+    }
+
+    
 }
