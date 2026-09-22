9e1b3ecad11c47264c772c576b1f034fd3229390
Your Name <your.email@example.com>
2026-09-22 13:44:59 -0600

    Added Job class

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Job
+{
+    public string _jobtitle;
+    public string _company;
+    public int _startyear;
+    public int _endyear;
+
+
+    public void Display()
+    {
+        
+        Console.WriteLine($"{_jobtitle} ({_company}) {_startyear}-{_endyear}");
+    }
+    
+
+
+}
